d8aed9ae0021fe40a505b2c48097b61c78ae2717
Luis Alho <luisalhopt@gmail.com>
2023-02-25 21:56:26 +0000

    remove print

--- src/main/java/net/luisalho/financemanager/FinanceManagerApplication.java
+++ src/main/java/net/luisalho/financemanager/FinanceManagerApplication.java
@@ -8,7 +8,6 @@ public class FinanceManagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinanceManagerApplication.class, args);
-		System.out.println("End");
 	}
 
 }
